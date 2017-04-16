570f92d4a9d5b21121a566a72e5aea97d2441f4a
Richard Loveland <ricky.loveland96@gmail.com>
2017-04-16 22:14:17 +0000

    the view of results work now

--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader
 from .models import Question, Choice
+from django.core.urlresolvers import reverse
 
 
 def index(request):
