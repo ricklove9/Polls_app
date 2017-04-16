e6ff9ea9333bd51a615ac18e0f4faded902e6660
Richard Loveland <ricky.loveland96@gmail.com>
2017-04-16 21:26:39 +0000

    fixed the views problem now it works properly

--- polls/views.py
+++ polls/views.py
@@ -6,7 +6,6 @@ from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('pools/index.html')
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 
